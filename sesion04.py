5157fb95c7992e2d70aed701aaf229a20466a957
DanielV <100451223@alumnos.uc3m.es>
2024-03-05 18:05:43 +0100

    changed sleep in button thread loop

--- sesion04.py
+++ sesion04.py
@@ -104,7 +104,8 @@ def button_thread():
     while True:
         if not GPIO.input(BUTTON_GPIO):
             power_on = not power_on
-        time.sleep(1)
+            time.sleep(0.5)
+        time.sleep(0.1)
 
 def luminosity_thread():
     global power_on
